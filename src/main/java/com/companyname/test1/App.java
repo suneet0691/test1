9f3f6b717d30134f454eaafd4cb20b145b84d4c1
suneet0691 <sunkaur@deloitte.com>
2017-03-13 13:25:14 +0530

    test
    test1

---
+++ src/main/java/com/companyname/test1/App.java
@@ -0,0 +1,13 @@
+package com.companyname.test1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
